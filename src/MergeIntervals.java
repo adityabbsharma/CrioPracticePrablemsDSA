c353e250014dc0a7fef7e258211eb5a89caba96c
adityabbsharma <adityabbsharma@gmail.com>
2021-08-14 14:11:14 +0530

    Used Stack-Given a collection of intervals, merge all overlapping intervals.

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -63,6 +63,95 @@ import java.util.*;
 class MergeIntervals {
 
     public int[][] mergeIntervals(int[][] intervals) {
+        // method 1 using prefix sum technique------------------------------------------
+//        Arrays.sort(intervals, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] a,
+//                               int[] b) {
+//
+//                return Integer.compare(a[0],b[0]);
+//            }
+//        });  // End of function call sort().
+//        int noOfIntervals = intervals.length;
+////        HashMap<Integer,Integer> hMap = new HashMap<>();
+//        HashSet<Integer> hSet = new HashSet<>();
+//        HashSet<Integer> hSet2 = new HashSet<>();
+//        int len = intervals[noOfIntervals-1][intervals[noOfIntervals-1].length-1];
+//        int[] arr = new int[len+2];
+//        arr[0] =0;
+//        for(int i=0;i<noOfIntervals;i++){
+//            if(intervals[i][0] == 0)
+//                arr[1] = 1;
+//            else{
+//                if(i>0){
+//                    if(intervals[i][0] == intervals[i-1][1] + 1){
+//                        hSet2.add(intervals[i-1][1]);
+//                        hSet.add(intervals[i][0]);
+////                        System.out.println("i m here");
+//                    }
+//                }
+//
+//                arr[intervals[i][0]] = 1;
+//            }
+//
+//            arr[intervals[i][1] + 1] = -1;
+////            hMap.put(intervals[i][0],intervals[i][1]);
+//        }
+////        System.out.println(hMap);
+////        for(int i=0;i<arr.length;i++){
+////            System.out.print(arr[i]+" ");
+////        }
+////        System.out.println();
+//        for(int i=1;i<arr.length;i++){
+//            arr[i] = arr[i] + arr[i-1];
+//        }
+////        for(int i=0;i<arr.length;i++){
+////            System.out.print(arr[i]+" ");
+////        }
+////        System.out.println();
+//        int noOfNewIntervals=0,flagStartOfAnInterVal=0;
+//        Queue<Integer> newIntervalsStartEndPoints = new LinkedList<>();
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i] !=0 && flagStartOfAnInterVal ==0 && !hSet.contains(i) ){
+//                flagStartOfAnInterVal = 1;
+//                noOfNewIntervals++;
+//                newIntervalsStartEndPoints.add(i);
+////                System.out.println("noOfNewIntervals here1="+noOfNewIntervals);
+//            }
+//            if(arr[i] !=0  && hSet.contains(i)){
+//                noOfNewIntervals++;
+//                flagStartOfAnInterVal = 1;
+//                newIntervalsStartEndPoints.add(i);
+////                System.out.println("noOfNewIntervals here2="+noOfNewIntervals);
+//            }
+//            if(arr[i] ==0 && flagStartOfAnInterVal==1){
+//                flagStartOfAnInterVal =0;
+//                newIntervalsStartEndPoints.add(i-1);
+////                System.out.println("noOfNewIntervals here3="+noOfNewIntervals);
+//            }
+//            if(arr[i] !=0 && hSet2.contains(i)){
+//                flagStartOfAnInterVal =0;
+//                newIntervalsStartEndPoints.add(i);
+////                System.out.println("noOfNewIntervals here4="+noOfNewIntervals);
+//            }
+//
+//            if(flagStartOfAnInterVal==1 && i==arr.length-1){
+//                if(arr[i]!=0){
+//                    newIntervalsStartEndPoints.add(i-1);
+////                    System.out.println("noOfNewIntervals here5="+noOfNewIntervals);
+//                }
+//            }
+//        }
+////        System.out.println("noOfNewIntervals="+noOfNewIntervals);
+//        int[][] newArr = new int[noOfNewIntervals][2];
+//        for(int i=0;i<noOfNewIntervals;i++){
+//            newArr[i][0] = newIntervalsStartEndPoints.remove();
+////            System.out.println(newArr[i][0] );
+//            newArr[i][1] = newIntervalsStartEndPoints.remove();
+////            System.out.println(newArr[i][1]);
+//        }
+//        return newArr;
+        // method 2 using array and sorted array
         Arrays.sort(intervals, new Comparator<int[]>() {
             @Override
             public int compare(int[] a,
@@ -70,86 +159,30 @@ class MergeIntervals {
 
                 return Integer.compare(a[0],b[0]);
             }
-        });  // End of function call sort().
-        int noOfIntervals = intervals.length;
-//        HashMap<Integer,Integer> hMap = new HashMap<>();
-        HashSet<Integer> hSet = new HashSet<>();
-        HashSet<Integer> hSet2 = new HashSet<>();
-        int len = intervals[noOfIntervals-1][intervals[noOfIntervals-1].length-1];
-        int[] arr = new int[len+2];
-        arr[0] =0;
-        for(int i=0;i<noOfIntervals;i++){
-            if(intervals[i][0] == 0)
-                arr[1] = 1;
-            else{
-                if(i>0){
-                    if(intervals[i][0] == intervals[i-1][1] + 1){
-                        hSet2.add(intervals[i-1][1]);
-                        hSet.add(intervals[i][0]);
-//                        System.out.println("i m here");
-                    }
-                }
-
-                arr[intervals[i][0]] = 1;
+        });
+        Stack<Integer> stack = new Stack<>();
+        stack.push(intervals[0][0]);
+        stack.push(intervals[0][1]);
+        for(int i=1;i< intervals.length;i++){
+            if(stack.peek()<intervals[i][0]){
+                stack.push(intervals[i][0]);
+                stack.push(intervals[i][1]);
             }
-
-            arr[intervals[i][1] + 1] = -1;
-//            hMap.put(intervals[i][0],intervals[i][1]);
-        }
-//        System.out.println(hMap);
-//        for(int i=0;i<arr.length;i++){
-//            System.out.print(arr[i]+" ");
-//        }
-//        System.out.println();
-        for(int i=1;i<arr.length;i++){
-            arr[i] = arr[i] + arr[i-1];
-        }
-//        for(int i=0;i<arr.length;i++){
-//            System.out.print(arr[i]+" ");
-//        }
-//        System.out.println();
-        int noOfNewIntervals=0,flagStartOfAnInterVal=0;
-        Queue<Integer> newIntervalsStartEndPoints = new LinkedList<>();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] !=0 && flagStartOfAnInterVal ==0 && !hSet.contains(i) ){
-                flagStartOfAnInterVal = 1;
-                noOfNewIntervals++;
-                newIntervalsStartEndPoints.add(i);
-//                System.out.println("noOfNewIntervals here1="+noOfNewIntervals);
-            }
-            if(arr[i] !=0  && hSet.contains(i)){
-                noOfNewIntervals++;
-                flagStartOfAnInterVal = 1;
-                newIntervalsStartEndPoints.add(i);
-//                System.out.println("noOfNewIntervals here2="+noOfNewIntervals);
-            }
-            if(arr[i] ==0 && flagStartOfAnInterVal==1){
-                flagStartOfAnInterVal =0;
-                newIntervalsStartEndPoints.add(i-1);
-//                System.out.println("noOfNewIntervals here3="+noOfNewIntervals);
-            }
-            if(arr[i] !=0 && hSet2.contains(i)){
-                flagStartOfAnInterVal =0;
-                newIntervalsStartEndPoints.add(i);
-//                System.out.println("noOfNewIntervals here4="+noOfNewIntervals);
-            }
-
-            if(flagStartOfAnInterVal==1 && i==arr.length-1){
-                if(arr[i]!=0){
-                    newIntervalsStartEndPoints.add(i-1);
-//                    System.out.println("noOfNewIntervals here5="+noOfNewIntervals);
-                }
+            else if(stack.peek()<intervals[i][1]){
+                stack.pop();
+                stack.push(intervals[i][1]);
             }
         }
-//        System.out.println("noOfNewIntervals="+noOfNewIntervals);
-        int[][] newArr = new int[noOfNewIntervals][2];
-        for(int i=0;i<noOfNewIntervals;i++){
-            newArr[i][0] = newIntervalsStartEndPoints.remove();
-//            System.out.println(newArr[i][0] );
-            newArr[i][1] = newIntervalsStartEndPoints.remove();
-//            System.out.println(newArr[i][1]);
+        Stack<Integer> q = new Stack<>();
+        while(!stack.isEmpty()){
+            q.push(stack.pop());
         }
-        return newArr;
+        int[][] newIntervals = new int[q.size()/2][2];
+        for(int i =0;i< newIntervals.length;i++){
+            newIntervals[i][0]=q.pop();
+            newIntervals[i][1]=q.pop();
+        }
+        return newIntervals;
     }
 
     public static void main(String[] args)
